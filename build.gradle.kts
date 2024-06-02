ee9fb8f8c493792d6995b4f7b239db17a271914d
Sahil Muthoo <sahil.muthoo@gmail.com>
2024-06-02 20:45:26 +1000

    switch to versions used by misk-jdbc

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,11 @@ repositories {
 dependencies {
     implementation("ch.qos.logback:logback-core:1.4.14")
     implementation("org.slf4j:slf4j-api:2.0.11")
-    implementation("com.mysql:mysql-connector-j:8.2.0")
-    implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("com.mysql:mysql-connector-j:8.0.33")
+    implementation("com.zaxxer:HikariCP:4.0.3")
 
     runtimeOnly("ch.qos.logback:logback-classic:1.4.14")
+
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
